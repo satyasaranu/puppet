c8ae2a0d89d8ac2f472f230b505df11ae4e18731
root <root@jenkinsnode01.saranu.com>
2016-03-03 00:23:08 -0800

    deleted

--- modules/om1234/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'om1234' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('om1234') }
-  end
-end
--- modules/om12345/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'om12345' do
-
-  context 'with defaults for all parameters' do
-    it { should contain_class('om12345') }
-  end
-end
